f65068ab5316b0c1989cf901981a979ac0bd11f0
Moeletsi-code06 <moeletsi440@gmail.com>
2026-09-16 22:21:39 +0200

    Sixth Commit

--- Access System project/Access System.py
+++ Access System project/Access System.py
@@ -32,68 +32,63 @@ accessLog = {
 #FUNCTION DEFINITIONS FOR RETRIEVAL OF THE ACCESS CODE,DENIAL OF UNAUTHORIZED ACCESS,SERVO ACCESS INDICATOR,LOGS AND MAINTENACE INDICATOR.
 def access_code():
     access = str(button1.value()) + str(button2.value()) + str(button3.value())    #Converts binary input into a string
-    utime.sleep(0.1)
+    utime.sleep(0.1)                                                               #Provides the button states time to not create duplicate entries
     return access                                                                  #returns the complete 3-bit binary access code
 
 def unauthorized():
     for i in range(50):
             buzzer.toggle()              #Toggles the buzzer to indicate room occupancy
             led1.toggle()				 #Toggles the LED  when motion is detected in the room
-            utime.sleep(0.1)
+            utime.sleep(0.1)             #Time delay method to allow the green led to blink every 0.1 seconds
             
             
 def authorized():
-    for i in range(10):             #for loop to allow the access granted green led to go on for 1 second(10 x 0.1)
-        led2.toggle()
-        utime.sleep(0.1)            #time delay method to allow the green led to blink every 0.1 seconds
+    for i in range(10):             #For loop to allow the access granted green led to go on for 1 second(10 x 0.1)
+        led2.toggle()               #Green LED switches on 
+        utime.sleep(0.1)            #Time delay method to allow the green led to blink every 0.1 seconds
    
 def servo_access():
-    servo.duty_u16(unlocked)
-    utime.sleep(2)             #time delay of 2 seconds before it changes states
-    servo.duty_u16(locked)
+    servo.duty_u16(unlocked)   #Moves the servo into a unlocked state
+    utime.sleep(2)             #Time delay of 2 seconds before it changes states
+    servo.duty_u16(locked)     #Moves the servo into a locked state 
     
 #Following function appends the formatted log entry data to the created log_book
-def logging_function():      #appending logs to created file 
-    access = -1
-    with open("accessLogs.txt","a") as file:#appends to access log text file
-        #displays access logs according to the number of entries:
-        #for access in range(access_entries):   
+def logging_function():                                #Appending log entries stored in the dictionary list to a text file.
+    access = -1                                        #Appends the last entry made in the dictionary list of log entries
+    with open("accessLogs.txt","a") as file:           #Appends to the access lof text file the formatted log entries
         file.write(f"{accessLog["LOG NO."][access]}{accessLog["BINARY CODE"][access]:>14}{accessLog['USER TYPE'][access]:>20}{accessLog["RESULT"][access]:>16}{accessLog["ACCESS COUNT"][access]:>15}{accessLog["Time"][access]:>12}\n")
     
     
-def maintenance_threshold(success):
+def maintenance_threshold(success):                          #Maintenance alert function
     global successful_access             
-    successful_access = success
-    threshold= 5
-    if successful_access == threshold:
-        lcd.clear()
-        lcd.message("MAINTENANCE")
+    successful_access = success                                         
+    threshold= 5                                            #Threshold of 5 successful entries 
+    if successful_access == threshold:                      #If the number of successful entries is equal to the threshold the following executes:
+        lcd.clear()                                         #Clears the lcd screen
+        lcd.message("MAINTENANCE")                          #Displays Maintenance required on the LCD 
         lcd.message("\nREQUIRED")
-        for i in range(30):
+        for i in range(30):                                 #Triggers the buzzer and led for a duration of 3 seconds
             buzzer.toggle()
             led1.toggle()
-            utime.sleep(1)
+            utime.sleep(1)                                  #buzzer and led trigger for 1 second each
         
-        
+
             
-authorised_codes = ["101","110","010", "001"]
-user_type = ["General Staff", "Academic Staff","Student", "Maintenance"]
-result = "DENIED"
-user = "UNAUTHORIZED USER"
+authorised_codes = ["101","110","010", "001"]                               #Authorised access code list
+user_type = ["General Staff", "Academic Staff","Student", "Maintenance"]    #Authorized users of the system list
 
 
-log_num = 0                   #log number counter intializer variable set to 0
-successful_access = 0 		  #Initialized counter to track how many successful accesses.
-#access_entries = 0            #Initialized counter to track how many entries in the system
-appending_completed = False   #Initializes while loop blocker for logging function
+
+log_num = 0                     #log number counter intializer variable set to 0
+successful_access = 0 		    #Initialized counter to track how many successful accesses.
+#access_entries = 0             #Initialized counter to track how many entries in the system
+appending_completed = False     #Initializes while loop blocker for logging function
 
 while True: 
     accessC = access_code()          		 #Receives the access code from the function access_code():
     current_time = utime.localtime()         #sets the current computer time with the utime library
     standard_format= "{:02d}:{:02d}:{:02d}".format(current_time[3], current_time[4], current_time[5])    #formats the current time of the device to be in standard time format.
     access_Time = standard_format  # sets the formatted access time in the access_time variable that will be looping to display different time values.
-#     log_num = log_num + 1      #Counter for the log number in the log entries
-#     accessLog["LOG NO."].append(log_num)   #appends the log entry number
     
     
     #.append() method is used to add log entries in the log book
@@ -103,8 +98,8 @@ while True:
         user = user_type[position]
         successful_access += 1 #Counts how many successful accesses in each loop.
         log_num = log_num + 1      #Counter for the log number in the log entries
-        accessLog["LOG NO."].append(log_num)   #appends the log entry number
-        #access_entries += 1    #Counts how many entries in each loop
+        accessLog["LOG NO."].append(log_num)   #appends the log entry number into the access log text file
+        
         
         lcd.clear()  
         lcd.message("ACCESS GRANTED")    			#Displays access granted on the LCD screen
@@ -142,68 +137,6 @@ while True:
         unauthorized()  						#Calls the unauthorized access function.
         
     
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
         
         
